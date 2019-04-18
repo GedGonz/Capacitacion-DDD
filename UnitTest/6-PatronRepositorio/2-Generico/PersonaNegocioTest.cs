8d27af98aa588cf8048e052262874704bba4f6ac
GedGonz <gedgonz7@gmail.com>
2019-04-17 21:59:53 -0600

    Nuevo Test

---
+++ UnitTest/6-PatronRepositorio/2-Generico/PersonaNegocioTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NegocioPRG.Dominio;
+using DatosPRG.Repositorio;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PersonaNegocioTest
+    {
+        [TestMethod]
+        public void ListarPersonasTest()
+        {
+            var PersonaRepositorio = new PersonaRepositorio();
+            var Resultad = new PersonaNegocio(PersonaRepositorio).ListarPersonas();
+
+            Assert.IsNotNull(Resultad);
+        }
+    }
+}
